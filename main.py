b722931955ce3c901377c4e37b753ac80b5bf58f
Dhanushnaayak <dhanush.nayak@nutanxt.com>
2023-07-29 13:09:49 +0530

    Data Ingestion Stage completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from mlProject.utils import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline,STAGE_NAME
 
-logger.info("Welcome to Custom Log")
+
+
+if __name__=='__main__':
+    try:
+        logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
